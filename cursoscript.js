c0b85197d46dc5dc859ce767ca194ef591b017d7
Alexandre Pedroso <alenoow@gmail.com>
2022-10-14 17:09:17 -0300

    Upload de Projeto de portfólio

---
+++ cursoscript.js
@@ -0,0 +1,66 @@
+//FUNÇÕES DE MARKETING //
+
+function inbound_mkt () {
+    let el = document.getElementById('meu-Modal');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+    }
+function modal_mktconteudo () {
+    let el = document.getElementById('mktconteudo');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+    }     
+function fundamentos_mkt () {
+    let el = document.getElementById('modal_fundamentos');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+    } 
+
+// FUNÇÕES DE RECURSOS HUMANOS // 
+
+function masterclass_rh () {
+    let el = document.getElementById('modal_masterclass');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+    }
+function rotinas_rh () {
+    let el = document.getElementById('modal_rotinas');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+    }     
+function controleponto_rh () {
+    let el = document.getElementById('modal_controleponto');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+}
+function pesquisa_organizacional () {
+    let el = document.getElementById('modal_pesquisa');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+}
+function introducao_esocial () {
+    let el = document.getElementById('modal_esocial');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+}
+function gestao_pessoas () {
+    let el = document.getElementById('modal_gestao');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+}
+function inteligencia_emocional () {
+    let el = document.getElementById('modal_inteligencia');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+}
+function introducao_logica () {
+    let el = document.getElementById('modal_introducaolog');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+}
+function costumer_sucess () {
+    let el = document.getElementById('modal_costumer');
+    let minha_modal = new bootstrap.Modal(el);
+    minha_modal.show();
+}
+    
